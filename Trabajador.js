345c899c4fbf32210bcf913eec0d58e169bbd4bf
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2019-10-19 16:48:33 +0200

    Game.js reorganizada y optimizada

--- Trabajador.js
+++ Trabajador.js
@@ -1,56 +1,50 @@
 import Unidad from "./Unidad.js";
 
 export default class Trabajador extends Unidad {
-    constructor(scene, positionx, positiony, game) {
+  constructor(scene, positionx, positiony, game) {
 
-      super(scene, positionx,positiony, 100, 1);
+    super(scene, positionx,positiony, 100, 1);
       
-      this.stats = {
-        type : 'empty',
-        position : {positionx, positiony}, 
-        owner: undefined,
-        selected: undefined,
-        image: undefined
+    this.stats = {
+      type : 'empty',
+      position : {positionx, positiony}, 
+      owner: undefined,
+      selected: undefined,
+      image: undefined
 
-        }
+      }
 
-      this.game = scene;  
+    this.game = scene;  
 
-    }
+  }
  
     
-    move(x,y){
-        //mover unidad seleccionada
-        if(this.game.tablero.casillas[y][x].stats.exists === true && this.game.tablero.casillas[y][x].stats.type !== 'mountain' &&  this.game.tablero.casillas[y][x].stats.type !== 'superMountain'&&
-          Math.abs(x-this.stats.position.positionx) <= 1 && Math.abs(y-this.stats.position.positiony) <= 1){
-          if (x>= 0 && x <= 10 && y >= 1 && y <= 7){
-              if(this.game.tablero.casillas[y][x].stats.unit === undefined){
+move(x,y){
+    //mover unidad seleccionada
+    if(this.game.tablero.casillas[y][x].stats.exists === true && this.game.tablero.casillas[y][x].stats.type !== 'mountain' &&  this.game.tablero.casillas[y][x].stats.type !== 'superMountain'&&
+      Math.abs(x-this.stats.position.positionx) <= 1 && Math.abs(y-this.stats.position.positiony) <= 1){
+      if (x>= 0 && x <= this.game.anchoMundo - 1 && y >= 1 && y <= this.game.altoMundo - 2){
+        if(this.game.tablero.casillas[y][x].stats.unit === undefined){
 
-
-                this.game.tablero.casillas[this.stats.position.positiony][this.stats.position.positionx].stats.unit = undefined;
+          this.game.tablero.casillas[this.stats.position.positiony][this.stats.position.positionx].stats.unit = undefined;
                 
-                this.game.tablero.casillas[y][x].stats.unit = this;
-
-                
-
-                this.stats.position.positionx = x;
-                this.stats.position.positiony = y;
-                this.stats.image.destroy();
-
-                this.stats.image = this.game.add.image(this.stats.position.positionx*this.squareSize + this.offset ,this.stats.position.positiony*this.squareSize + this.offset ,'worker');
-
-
-
-                this.game.tablero.casillas[y][x].stats.unit.stats.selected = false;
-
-                this.game.selected = undefined;
-                this.game.flecha.exists = false;
-                this.game.flecha.image.destroy();
-
+          this.game.tablero.casillas[y][x].stats.unit = this;
 
                 
-              }
-          }
+          this.stats.position.positionx = x;
+          this.stats.position.positiony = y;
+          this.stats.image.destroy();
+
+          this.stats.image = this.game.add.image(this.stats.position.positionx*this.squareSize + this.game.offset ,this.stats.position.positiony*this.squareSize + this.game.offset ,'worker');
+
+
+          this.game.tablero.casillas[y][x].stats.unit.stats.selected = false;
+
+          this.game.selected = undefined;
+          this.game.flecha.exists = false;
+          this.game.flecha.image.destroy();  
         }
       }
     }
+  }
+}
--- game.js
+++ game.js
@@ -31,13 +31,13 @@ export default class Game extends Phaser.Scene {
       this.mouse = this.input.activePointer;
      
 
-      //Trabajadora de prueba ||DEBUG||
-      this.workers.push(new Trabajador(this, 5, 4));
-      this.tablero.casillas[4][5].stats.unit = this.workers[this.workers.length-1]
+      //-------------------------Trabajadora de prueba ||DEBUG||----------------------------
+      this.workers.push(new Trabajador(this, 5, 5));
+      this.tablero.casillas[5][5].stats.unit = this.workers[this.workers.length-1]
       this.printWorkers();
-      //
+      //------------------------------------------------------------------------------------
 
-      this.selected = undefined;
+      this.selectedUnit = undefined;
 
       this.flecha ={
         positionx : undefined,
@@ -50,26 +50,28 @@ export default class Game extends Phaser.Scene {
 
   }
 
-    update(time, delta) {
+  update(time, delta) {
       
-      this.moveSelected();
+    this.checkForSelection();
 
-      this.checkForSelection();
+    this.printArrow(); 
+
+    this.moveSelected();
       
-      this.printArrow();   
+      
 
-    }
+  }
 
-    loadImages(){
+  loadImages(){
       //Casillas
       this.loadCasillas();
       
       //Unidad(es)
       this.load.image('worker',' assets/imagenes/worker.png' );
       this.load.image('workerSelected',' assets/imagenes/workerSelected.png' );
-    }
+  }
 
-    loadCasillas(){
+  loadCasillas(){
       //Tipos de casilla
       this.load.image('casilla', 'assets/imagenes/Casilla.png');
       this.load.image('casillaRed', 'assets/imagenes/CasillaRed.png');
@@ -86,96 +88,96 @@ export default class Game extends Phaser.Scene {
       this.load.image('arrowdownright', 'assets/imagenes/arrowdownright.png');
       this.load.image('arrowupleft', 'assets/imagenes/arrowupleft.png');
       this.load.image('arrowdownleft', 'assets/imagenes/arrowdownleft.png');
-    }
+  }
     
-    checkForSelection(){
-      if(this.mouse.leftButtonDown()){
+  checkForSelection(){
+    if(this.mouse.leftButtonDown()){
 
+      let x = Math.floor(this.mouse.worldX/this.squareSize -1);
+      let y = Math.floor(this.mouse.worldY/this.squareSize -1);
+
+      if (x>= 0 && x <= this.anchoMundo - 1 && y >= 1 && y <= this.altoMundo - 2){
+          if(this.tablero.casillas[y][x].stats.unit !== undefined){
+            if (this.selectedUnit !== undefined) this.selectedUnit.stats.selected = false;    //OJO
+            this.tablero.casillas[y][x].stats.unit.stats.selected = true;
+            this.selectedUnit = this.tablero.casillas[y][x].stats.unit;
+
+            this.selectedUnit.stats.image.destroy();
+            this.selectedUnit.stats.image = this.add.image(this.selectedUnit.stats.position.positionx*this.squareSize + this.offset ,this.selectedUnit.stats.position.positiony*this.squareSize + this.offset ,'workerSelected');
+          }
+          // else{
+          //   this.selected = undefined;
+          //   this.flecha.image.destroy();
+          // }
+
+      }
+    }
+
+    if(this.mouse.rightButtonDown()){
+
+      if (this.selectedUnit !== undefined){
+      this.selectedUnit.stats.image.destroy();
+      this.selectedUnit.stats.image = this.add.image(this.selectedUnit.stats.position.positionx*this.squareSize + this.offset ,this.selectedUnit.stats.position.positiony*this.squareSize + this.offset ,'worker');
+      }
+      this.selectedUnit = undefined;
+      this.flecha.image.destroy();
+    }
+
+  }
+
+  moveSelected(){
+    if (this.selectedUnit !== undefined && this.mouse.leftButtonDown()){
+      //mover unidad seleccionada
+      let x = Math.floor(this.mouse.worldX/this.squareSize -1);
+      let y = Math.floor(this.mouse.worldY/this.squareSize -1);
+
+      if (x < this.anchoMundo && x >= 0 && y >= 0 && y < this.altoMundo)
+      this.selectedUnit.move(x,y);
+      
+    }
+  }
+
+  // printTablero(){
+  //   this.tablero.printTablero();
+  // }
+
+  printArrow(){
+      if (this.selectedUnit !== undefined){
         let x = Math.floor(this.mouse.worldX/this.squareSize -1);
         let y = Math.floor(this.mouse.worldY/this.squareSize -1);
         if (x>= 0 && x <= this.anchoMundo - 1 && y >= 1 && y <= this.altoMundo - 2){
-            if(this.tablero.casillas[y][x].stats.unit !== undefined){
-              this.tablero.casillas[y][x].stats.unit.stats.selected = true;
-              this.selected = this.tablero.casillas[y][x].stats.unit;
-
-              this.selected.stats.image.destroy();
-              this.selected.stats.image = this.add.image(this.selected.stats.position.positionx*this.squareSize + this.offset ,this.selected.stats.position.positiony*this.squareSize + this.offset ,'workerSelected');
-
-            }
-            // else{
-            //   this.selected = undefined;
-            //   this.flecha.image.destroy();
-            // }
-
-        }
-      }
-      if(this.mouse.rightButtonDown()){
-
-        if (this.selected !== undefined){
-        this.selected.stats.image.destroy();
-        this.selected.stats.image = this.add.image(this.selected.stats.position.positionx*this.squareSize + this.offset ,this.selected.stats.position.positiony*this.squareSize + this.offset ,'worker');
-        this.tablero.casillas[y][x].stats.unit.stats.selected = false;
-      }
-        this.selected = undefined;
-        this.flecha.image.destroy();
-
-
-      }
-
-    }
-
-    moveSelected(){
-      if (this.selected !== undefined){
-        let x = Math.floor(this.mouse.worldX/this.squareSize -1);
-        let y = Math.floor(this.mouse.worldY/this.squareSize -1);
-        //mover unidad seleccionada
-        if(this.mouse.leftButtonDown()){
-            this.selected.move(x,y);
-      }
-    }
-    }
-
-    // printTablero(){
-    //   this.tablero.printTablero();
-    // }
-
-    printArrow(){
-      if (this.selected !== undefined){
-        let x = Math.floor(this.mouse.worldX/this.squareSize -1);
-        let y = Math.floor(this.mouse.worldY/this.squareSize -1);
-        if (x>= 0 && x <= 10 && y >= 1 && y <= 7){
 
           //Pintar flecha de casilla a la que mover
 
           if(this.tablero.casillas[y][x].stats.exists === true && this.tablero.casillas[y][x].stats.type !== 'mountain' &&  this.tablero.casillas[y][x].stats.type !== 'superMountain'&&
-          Math.abs(x-this.selected.stats.position.positionx) <= 1 && Math.abs(y-this.selected.stats.position.positiony) <= 1){
+          Math.abs(x-this.selectedUnit.stats.position.positionx) <= 1 && Math.abs(y-this.selectedUnit.stats.position.positiony) <= 1){
 
-            if ((this.flecha.positionx !== x || this.flecha.positiony !== y) && x === this.selected.stats.position.positionx ){
-              if (y < this.selected.stats.position.positiony){
+            if ((this.flecha.positionx !== x || this.flecha.positiony !== y) && x === this.selectedUnit.stats.position.positionx ){
+              if (y < this.selectedUnit.stats.position.positiony){
                 
                
                 if (this.flecha.image !== undefined) {this.flecha.image.destroy();}
                 this.flecha.image = this.add.image(x*this.squareSize + this.offset ,y*this.squareSize + this.offset ,'arrowup');
               }
 
-              else if (y > this.selected.stats.position.positiony){
+              else if (y > this.selectedUnit.stats.position.positiony){
                 if (this.flecha.image !== undefined) {this.flecha.image.destroy();}
                 this.flecha.image = this.add.image(x*this.squareSize + this.offset ,y*this.squareSize + this.offset ,'arrowdown');
 
               }
             }
-            else if ((this.flecha.positionx !== x || this.flecha.positiony !== y) && (x > this.selected.stats.position.positionx ) ){
-              if (y === this.selected.stats.position.positiony){
+            else if ((this.flecha.positionx !== x || this.flecha.positiony !== y) && (x > this.selectedUnit.stats.position.positionx ) ){
+              if (y === this.selectedUnit.stats.position.positiony){
                 if (this.flecha.image !== undefined) {this.flecha.image.destroy();}
                 this.flecha.image = this.add.image(x*this.squareSize + this.offset ,y*this.squareSize + this.offset ,'arrowright');
 
               }
-              else if (y < this.selected.stats.position.positiony){
+              else if (y < this.selectedUnit.stats.position.positiony){
                 if (this.flecha.image !== undefined) {this.flecha.image.destroy();}
                 this.flecha.image = this.add.image(x*this.squareSize + this.offset ,y*this.squareSize + this.offset ,'arrowupright');
 
               }
-              else if (y > this.selected.stats.position.positiony){
+              else if (y > this.selectedUnit.stats.position.positiony){
                 if (this.flecha.image !== undefined) {this.flecha.image.destroy();}
                 this.flecha.image = this.add.image(x*this.squareSize + this.offset ,y*this.squareSize + this.offset ,'arrowdownright');
 
@@ -183,18 +185,18 @@ export default class Game extends Phaser.Scene {
 
             }
 
-            else if ((this.flecha.positionx !== x || this.flecha.positiony !== y) && x < this.selected.stats.position.positionx ){
-              if (y === this.selected.stats.position.positiony){
+            else if ((this.flecha.positionx !== x || this.flecha.positiony !== y) && x < this.selectedUnit.stats.position.positionx ){
+              if (y === this.selectedUnit.stats.position.positiony){
                 if (this.flecha.image !== undefined) {this.flecha.image.destroy();}
                 this.flecha.image = this.add.image(x*this.squareSize + this.offset ,y*this.squareSize + this.offset ,'arrowleft');
 
               }
-              else if (y < this.selected.stats.position.positiony){
+              else if (y < this.selectedUnit.stats.position.positiony){
                 if (this.flecha.image !== undefined) {this.flecha.image.destroy();}
                 this.flecha.image = this.add.image(x*this.squareSize + this.offset ,y*this.squareSize + this.offset ,'arrowupleft');
 
               }
-              else if (y > this.selected.stats.position.positiony){
+              else if (y > this.selectedUnit.stats.position.positiony){
                 if (this.flecha.image !== undefined) {this.flecha.image.destroy();}
                 this.flecha.image = this.add.image(x*this.squareSize + this.offset ,y*this.squareSize + this.offset ,'arrowdownleft');
 
@@ -205,14 +207,14 @@ export default class Game extends Phaser.Scene {
         }
 
       }
-    }
+  }
 
 
-    //Esto se usa una vez. Solo utilizar en debug. Mala idea.
-    printWorkers(){
+  //Esto se usa una vez. Solo utilizar en debug. Mala idea.
+  printWorkers(){
       for (let i = 0; i < this.workers.length; i++){
         this.workers[i].stats.image = this.add.image(this.workers[i].stats.position.positionx*this.squareSize + this.offset ,this.workers[i].stats.position.positiony*this.squareSize + this.offset ,'worker');
       }
-    }
+  }
 
 }
